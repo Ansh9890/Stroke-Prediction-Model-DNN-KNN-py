37515104b70c16f057b76cb0dc68739f4868b37a
Ansh9890 <115436132+Ansh9890@users.noreply.github.com>
2025-05-28 13:44:59 -0700

    use scale pre-processing on data training file csv, use train validation test splits

--- code.py
+++ code.py
@@ -15,3 +15,8 @@ df[categorical_cols] = df[categorical_cols].apply(LabelEncoder().fit_transform)
 df['bmi'].fillna(df['bmi'].mean(), inplace=True)
 X = df.drop(columns=['id', 'stroke'])
 y = df['stroke']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_train, X_temp, y_train, y_temp = train_test_split(X_scaled, y, test_size=0.36, random_state=42, stratify=y)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=5/9, random_state=42, stratify=y_temp)
